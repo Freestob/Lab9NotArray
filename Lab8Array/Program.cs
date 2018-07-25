6ff1cc731aa94e74c2a084b658e6fdf8c3bd8ab9
Bradley Freestone <bradley.freestone@gmail.com>
2018-07-25 17:30:58 -0400

    Redid how I did list

--- Lab8Array/Program.cs
+++ Lab8Array/Program.cs
@@ -11,14 +11,19 @@ namespace Lab8Array
 {
     class Program
     {
+
         
-         
         static void Main(string[] args)
         {
-            List<List<string>> _classmates = new List<List<string>>()
+            List<List<string>> studentInformation = new List<List<string>>()
             {
-            new List<string>() { "Billy Bob Thorton","Washington DC"," Chewing bubblegum", "Settlers of Catan"},
-            new List<string>() { "Lester Neigard", "Grand Rapids", "Taking names", "Monopoly" },
+                (new List<string>() {"Billy Bob Thorton", "Lester Neigard", "Peggy Valone", "Steve Wizouski", "Mike Sullivan", "Alvin Collins", "Charlie Humpkins", "Delta Flyer", "Eugine Elward", "Frank Lloyde-Wright"}),
+                (new List<string>() { "Washington DC", "Grand Rapids", "Portland", "Chicago", "Marquette", "the South Side", "the North Pole", "Whoville", "where you're from", "Holland"}),
+                (new List<string>() {"Chewing bubblegum", "Taking names","cashing checks", "hopschotch","armed robery", "throwing rocks at trains","absolutely nothing", "running","looting","arsen"}),
+                (new List<string>() {"Settlers of Catan", "Monopoly", "Betrayal at the House on the Hill", "Above and Below", "Life", "Dungeons and Dragons", "Near and Far", "Monster Hunter", "Mary Me Mister Darcy", "7 Wonders"}
+
+                new List<string>() { "Billy Bob Thorton","Washington DC"," Chewing bubblegum", "Settlers of Catan"},
+            new List<string>() { "Lester Neigard", "Grand Rapids", "Taking names", "Monopoly", },
             new List<string>() { "Peggy Valone", "Portland", "cashing checks", "Betrayal at the House on the Hill" },
             new List<string>() { "Steve Wizouski", "Chicago", "hopschotch", "Above and Below" },
             new List<string>() { "Mike Sullivan", "Marquette", "armed robery", "Life" },
@@ -27,67 +32,49 @@ namespace Lab8Array
             new List<string>() { "Delta Flyer", "Whoville", "running", "Monster Hunter" },
             new List<string>() { "Eugine Elward", "where you're from", "looting", "Mary Me Mister Darcy" },
             new List<string>() { "Frank Lloyde-Wright", "Holland", "arsen", "7 Wonders" },
-        };
-           
-            
+            };
+
+            var studentNames = 
 
             Console.WriteLine("As the new teacher in school it is wise to learn up on who you'll be teaching.");
-            Console.WriteLine("Which student do you want to learn about? Enter a number 1-20");
 
-            int userInput = int.Parse(Console.ReadLine());
-            int i = userInput - 1;
-            var student = _classmates[i];
-            Console.WriteLine($"This is {student[0]}");
-            Console.WriteLine("Would you like to know more about their hometown, talents, or boardgames? If yes please select type (hometown), (talents), or (boardgames).");
-            var userChoice = Console.ReadLine();
-            Console.ReadKey();
-
-            if (userChoice == "hometown")
+            
+            Console.WriteLine("Would you like to learn about a student or add a student? (learn) (add)");
+            if (Console.ReadLine() == "learn")
             {
-                Console.WriteLine($"{student[0]} is from {student[1]}");
+                Console.WriteLine("Which student would you like to learn about? Enter a number between 1-10");
+                int userInput = int.Parse(Console.ReadLine());
+                int i = userInput - 1;
+                var student = classmates[i];
+                Console.WriteLine($"This is {student[0]}");
+                Console.WriteLine("Would you like to know more about their hometown, talents, or boardgames? If yes please select type (hometown), (talents), or (boardgames).");
+                var userChoice = Console.ReadLine();
                 Console.ReadKey();
-            }
-            else if (userChoice == "talents")
-            {
-                Console.WriteLine($"{student[0]} is good at {student[2]}");
-            }
-            else if (userChoice == "boardgames")
-            {
-                Console.WriteLine($"{student[0]} likes to play {student[3]}");
-            }
-            else
-            {
-                Console.WriteLine($"That is not a valid input. That seems like something that {student[8]} would do.");
-            }
 
-
-
-            /*if (userChoice == "hometown")
-            {
-                Console.WriteLine(_classmates[i] + " is from " + _hometown[i]);
-                Console.WriteLine("Would you like to know about their talents? (y/n)");
-                if (Console.ReadLine().ToLower() == "y")
+                if (userChoice == "hometown")
                 {
-                    Console.WriteLine(_classmates[i] + " is good at " + _talents[i]);
+                    Console.WriteLine($"{student[0]} is from {student[1]}");
                     Console.ReadKey();
-                    return;
+                }
+                else if (userChoice == "talents")
+                {
+                    Console.WriteLine($"{student[0]} is good at {student[2]}");
+                }
+                else if (userChoice == "boardgames")
+                {
+                    Console.WriteLine($"{student[0]} likes to play {student[3]}");
+                }
+                else
+                {
+                    Console.WriteLine($"That is not a valid input. That seems like something that {student[8]} would do.");
                 }
             }
-            else if (userChoice == "talents")
+            else if(Console.ReadLine()=="add")
             {
-                Console.WriteLine(_classmates[i] + "is good at " + _talents[i]);
-                Console.WriteLine("Would you like to know about their hometown? (y/n)");
-                if (Console.ReadLine().ToLower() == "y")
-                {
-                    Console.WriteLine(_classmates[i] + " is from " + _hometown[i]);
-                    Console.ReadKey();
-                    return;
-                }
+             
             }
-            else
-            {
-                Console.WriteLine("That is not a valid answer");
-            }*/
         }
+
+        
     }
 }
